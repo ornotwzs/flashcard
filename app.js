54c9cdd61f9dff08e279d66589fd5424a75dfef6
ornotwzs <149188853+ornotwzs@users.noreply.github.com>
2024-10-15 15:37:45 -0300

    ikty

--- app.js
+++ app.js
@@ -14,3 +14,12 @@ function criaCartao(categoria, pergunta, resposta) {
     </div>
     </div>
     `
+
+    let respostaEstaVisivel= false
+
+function viraCartao(){
+    respostaEstaVisivel=respostaEstaVisivel
+    cartao.classList.toggle('activate'  respostaEstaVisivel)
+    }
+    cartao.addEventListener('click'viraCartao)
+    container.appendChild(cartao)
