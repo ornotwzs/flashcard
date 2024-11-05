d0a52d67679a6c85ce9f306faa950ab06f2715c6
ornotwzs <149188853+ornotwzs@users.noreply.github.com>
2024-11-05 15:21:41 -0300

    ghtfujjjjjjjjjjjjjj

--- perguntas.js
+++ perguntas.js
@@ -1,53 +1,53 @@
 criaCartao(
-    'Biologia',
+    'Genética',
     'O que é a Primeira Lei de Mendel?',
     'A Primeira Lei de Mendel afirma que cada indivíduo tem dois alelos por característica, herdados de pai e mãe, que se separam durante a formação dos gametas, garantindo a variabilidade genética.'
 )
 
 criaCartao(
-   'Biologia',
+   'Genética',
     'Como funciona a Primeira Lei de Mendel em um exemplo simples?',
     'Cruzando plantas de ervilha amarelas (AA) com verdes (aa), a primeira geração (F1) terá sementes amarelas (Aa). Na segunda geração (F2), a proporção será 3 amarelas para 1 verde.'
     )
 
 criaCartao(
-    'Biologia',
+    'Genética',
     'Como a ideia de segregação independente se relaciona com a Primeira Lei de Mendel?',
     'A Segunda Lei de Mendel aborda a segregação independente de diferentes características. A Primeira Lei também implica que os genes para uma característica se separam independentemente durante a formação dos gametas.')
 
 criaCartao(
-    'Biologia',
+    'Genética',
     'Qual a Segunda Lei de Mendel?',
     'A Segunda Lei de Mendel afirma que alelos de diferentes genes se segregam independentemente durante a formação dos gametas, significando que a herança de um gene não afeta a de outro gene.'
 )
 
 criaCartao(
-    'Biologia',
+    'Genética',
     'Como Mendel descobriu a Segunda Lei?',
     'Mendel, ao estudar ervilhas em cruzamentos diíbridos, notou que a herança de uma característica não influenciava a outra. Assim, ele concluiu que os alelos de diferentes genes se segregam independentemente.'
 )
 
 criaCartao(
-    'Biologia',
+    'Genética',
     'Considere uma planta de ervilha que é heterozigota para duas características: cor da semente (amarela é dominante sobre verde) e forma da semente (lisa é dominante sobre rugosa). Qual será a proporção fenotípica esperada na geração F2?',
     '9:3:3:1 (9 amarelas lisas : 3 amarelas rugosas : 3 verdes lisas : 1 verde rugosa).'
 )
 
 
 criaCartao(
-    'Biologia',
+    'Genética',
     'Oque é Co-odominância?',
     'Co-dominância é um padrão de herança onde ambos os alelos de um par são expressos igualmente no fenótipo. Um exemplo é o tipo sanguíneo AB, que combina características dos alelos A e B.'
 )
 
 criaCartao(
-    'Biologia',
+    'Genética',
     'Oque é Heredogramas?',
     'Heredogramas são representações gráficas que mostram a herança de características genéticas em famílias ao longo de gerações, permitindo identificar padrões de herança e a presença de doenças genéticas.'
 )
 
 criaCartao(
-    'Biologia',
+    'Genética',
     'Qual é a diferença entre Genótipo e fenótipo?',
     'Genótipo é a composição genética de um organismo, representando a combinação de alelos para um gene. Fenótipo, por outro lado, são as características observáveis resultantes da interação entre o genótipo e o ambiente, como cor, forma e comportamento.'
 )
